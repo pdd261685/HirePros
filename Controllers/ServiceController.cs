8d2adf1f350bc4f696babb80a489f3205bdad20f
Pooja Dwivedi <dwivedi.pooja0@gmail.com>
2020-01-26 15:48:33 -0600

    Removed Identity area

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HirePros.Data;
+using HirePros.Models;
+using HirePros.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HirePros.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly ServiceDbContext context;
+
+        public ServiceController(ServiceDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        //Get Controller
+        public IActionResult Index()
+        {
+            
+            List<Services> servs = context.Services.ToList();
+            return View(servs);
+            
+            //return Redirect("User/Index?username=" + HttpContext.Session.GetString("UserName"));
+        }
+
+        //Get Controller
+        public IActionResult Add()
+        {
+            if (HttpContext.Session.GetString("UserName") == "Admin")
+            {
+                AddServiceViewModel addServiceViewModel = new AddServiceViewModel();
+                return View(addServiceViewModel);
+            }
+            return Redirect("/User/Index?username=" + HttpContext.Session.GetString("UserName"));
+
+
+        }
+
+        //Post Controller
+
+        [HttpPost]
+        public IActionResult Add(AddServiceViewModel addServiceViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Services newService = new Services
+                {
+                    ServiceName = addServiceViewModel.ServiceName,
+                    ServiceDesc = addServiceViewModel.ServiceDesc
+                };
+
+                context.Services.Add(newService);
+                context.SaveChanges();
+
+                return Redirect("/Service");
+            }
+
+            return View(addServiceViewModel);
+        }
+    }
+}
